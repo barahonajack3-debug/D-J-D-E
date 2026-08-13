285cd90d6e00b78f3ceda3a23e1d5cdd70f10ade
Daniel <danielariassolano@gmail.com>
2026-08-13 15:27:18 -0600

    se agregaron metodos a la clase appointments

--- src/appointments/appointmentsList.java
+++ src/appointments/appointmentsList.java
@@ -4,10 +4,40 @@
  */
 package appointments;
 
+import clinic.KeyDynamicsLists;
+import java.util.TreeSet;
+
 /**
  *
  * @author Usuario
  */
-public class appointmentsList {
-    
+public class appointmentsList implements KeyDynamicsLists<Appointment, String>{
+    TreeSet<Appointment> appointments;
+
+    @Override
+    public Appointment get(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean remove(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean add(Appointment item) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+            
+            
 }
