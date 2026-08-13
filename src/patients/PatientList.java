bb870ef58e120ab48ee2d2445ecfafefc3eb77b8
EMMAXZZ <emmnuelpinda@gmail.com>
2026-08-13 14:59:07 -0600

    se añadio cambios a patientlist

--- src/patients/PatientList.java
+++ src/patients/PatientList.java
@@ -4,10 +4,46 @@
  */
 package patients;
 
+import clinic.KeyDynamicsLists;
+import java.util.HashMap;
+import java.util.Iterator;
+
 /**
  *
  * @author EMMAXZZ
  */
-public class PatientList {
-    
+public class PatientList implements KeyDynamicsLists<Patient,String>{
+    private HashMap<String,Patient> Patient;
+
+    @Override
+    public Patient get(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean remove(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Iterator getALL() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean add(Patient item) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+
 }
