f433b3a1ff081885941a7589508f964eda250a82
EMMAXZZ <emmnuelpinda@gmail.com>
2026-08-16 15:11:29 -0600

    2.0 se creo el frmpatient y paquete icons
    se añadieron cosas pequeñas como botones, textos y imagenes

--- src/patients/FrmPatient.java
+++ src/patients/FrmPatient.java
@@ -18,7 +18,7 @@ public class FrmPatient extends javax.swing.JFrame {
     public FrmPatient() {
         initComponents();
     }
-
+//arreglando error
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
